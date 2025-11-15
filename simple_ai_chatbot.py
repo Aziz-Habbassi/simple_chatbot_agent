9de4bc01e6ab7bc2e4e444b5489e9e7ba8286b88
Aziz Habbassi <azizhabbassi.online@gmail.com>
2025-11-15 10:31:29 +0100

    add webUi using streamlit

--- simple_ai_chatbot.py
+++ simple_ai_chatbot.py
@@ -1,28 +1,37 @@
 from langchain_ollama import OllamaLLM
 from langchain_community.chat_message_histories import ChatMessageHistory
 from langchain_core.prompts import PromptTemplate
+import streamlit as st
+
 
 llm=OllamaLLM(model="llama3.1:8b")# or choose the model you want
 
-chat_history=ChatMessageHistory()
+if "chat_history" not in st.session_state :
+    st.session_state.chat_history=ChatMessageHistory()
 prompt=PromptTemplate(input_variables=["chat_history","question"],template="Previous conversation: {chat_history}\nUser: {question}\nAI:")
 
 def run_chain(question):
-    chat_history_text="\n".join([f"{msg.type.capitalize()}: {msg.content}" for msg in chat_history.messages])
+    chat_history_text="\n".join([f"{msg.type.capitalize()}: {msg.content}" for msg in st.session_state.chat_history.messages])
 
     response = llm.invoke(prompt.format(chat_history=chat_history_text,question=question))
 
-    chat_history.add_user_message(question)
-    chat_history.add_ai_message(response)
+    st.session_state.chat_history.add_user_message(question)
+    st.session_state.chat_history.add_ai_message(response)
 
     return response
 
-print("🤖 Welcome to the AI Chatbot!\nIf you want to quit just enter 'exit' to end the conversation")
+st.title("🤖 Welcome to the AI Chatbot!")
+st.write("Ask me Anythin , if you want to quit just enter 'exit' to end the conversation")
 
-while True:
-    question=input("You:\n")
-    if question.lower()=="exit":
-        print("Have a nice Day , GoodBye !")
-        break
-    response = run_chain(question=question)
-    print("\n🤖AI : "+response)
+
+question=st.text_input("Your Question : ")
+if question.lower()=="exit":
+    st.write("🤖 Have a nice Day , GoodBye !")
+
+response = run_chain(question=question)
+st.write("You : "+question)
+st.write("🤖 AI : "+response)
+
+st.subheader("📜Chat History")
+for msg in st.session_state.chat_history.messages:
+    st.write(msg.type.capitalize()+" : "+msg.content)
